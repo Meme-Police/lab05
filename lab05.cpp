15238f7286a2d2b245e1cf080fe695ed7b13222b
bcdyas <66280720+bcdyas@users.noreply.github.com>
2022-02-04 10:07:48 -0700

    Added code styling to the source code

--- lab05.cpp
+++ lab05.cpp
@@ -1,5 +1,18 @@
-﻿// lab05.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿/***********************************************************************
+ * Program: W05 LAB HOMOGRAPHS
+ *    Week 05, HOMOGRAPHS
+ *    Brother Wilson, CSE 453
+ * Author:
+ *    Ethan Nelson, Josh Donaldson, Benjamin Dyas, Erik Machuca Saez 
+ * Summary:
+ *    Compare two strings of file paths to determine if they are
+ * 	homographs or not.
+ *    Estimated: 6 hrs each
+ *    Actual: 7 hrs each
+ * The difficulty was was with using c++. We were a little rusty with this
+ * language. Implementing the functions to determine the homographs was
+ * straightforward.
+ ************************************************************************/
 
 #include <iostream>
 #include <list>
@@ -9,6 +22,9 @@
 #include <algorithm>
 using namespace std;
 
+/***********************************************************************
+ * testCase class to hold the strings of the different test cases.
+ ***********************************************************************/
 struct testCase
 {
 	testCase(string a, string b, string d)
@@ -22,7 +38,7 @@ struct testCase
 	string d;
 };
 
-// PUT TEST CASES HERE
+// variables
 vector<testCase> testCases = {
 	testCase("C:\\secret\\password.txt", ".\\..\\..\\secret\\password.txt", "C:\\users\\public"),
 	testCase("C:\\doccuments\\bank_info\\bank_number.txt", ".\\bank_info\\banknumber.txt", "D:\\other\\trip")
@@ -30,11 +46,20 @@ vector<testCase> testCases = {
 
 // { {"C:\\secret\\password.txt", ".\\..\\..\\secret\\password.txt", "C:\\users\\public"} };
 
+// methods
 void rendering(string e) { cout << e; }
 list<string> canonicalization(string e, string d);
 bool homograph(string a, string b, string d);
 list<string> stringListConversion(string s);
 
+
+/******************************************************
+ * METHOD: homograph
+ * receives three strings to compare and determine if
+ * they are homographs
+ *   INPUT:  (3) strings
+ *   OUTPUT: boolean
+ ******************************************************/
 bool homograph(string a, string b, string d)
 {
 	// string a is the first filepath, b is the seccond, and d is the current directory
@@ -46,7 +71,14 @@ bool homograph(string a, string b, string d)
 	return false;
 }
 
-string get(list<string> l, int index)
+/******************************************************
+ * METHOD: getString
+ * receives a list of strings and index to
+ * return a string.
+ *   INPUT:  list of strings, int
+ *   OUTPUT: boolean
+ ******************************************************/
+string getString(list<string> l, int index)
 {
 	auto it = l.begin();
 	for (int i = 0; i < index; i++)
@@ -54,22 +86,33 @@ string get(list<string> l, int index)
 	return *it;
 }
 
+/******************************************************
+ * METHOD: canonicalization
+ * Uses the following two cannonisation rules to
+ * cannonize the strings into a list of of strings:
+ * 
+ * Unique Cannonisation Rule
+ * ∀ e1, e2 C(e1) ≠ C(e2) ↔ H(e1, e2) < p
+ * For all pairs of encodings, if they are not homographs,
+ * they must have different cannonisation
+ * 
+ * Reliable Cannonisation Rule
+ * ∀ e1, e2 C(e1) = C(e2) ↔ H(e1, e2) ≥ p
+ * For all pairs of encodings, if they are homographs,
+ * the cannonisation must be the same
+ * 
+ *   INPUT:  (2) strings
+ *   OUTPUT: list<string>
+ ******************************************************/
 list<string> canonicalization(string e, string d)
 {
-	// Unique Cannonisation Rule
-	// ∀ e1, e2 C(e1) ≠ C(e2) ↔ H(e1, e2) < p
-	// For all pairs of encodings, if they are not homographs, they must have different cannonisation
-
-	// Reliable Cannonisation Rule
-	// ∀ e1, e2 C(e1) = C(e2) ↔ H(e1, e2) ≥ p
-	// For all pairs of encodings, if they are homographs, the cannonisation must be the same
 	list<string> listE = stringListConversion(e);
 	list<string> listD = stringListConversion(d);
 	list<string> listC = {};
 	string c = "";
 	for (int i = 0; i < listE.size(); i++)
 	{
-		string x = get(listE, i);
+		string x = getString(listE, i);
 		if (x == ".")
 			listC = listD;
 		else if (x == "..")
@@ -92,6 +135,14 @@ list<string> canonicalization(string e, string d)
 	return listC;
 }
 
+/******************************************************
+ * METHOD: stringListConversion
+ * receives a file path string and breaks the string
+ * apart from the "\" operator and creates a list of
+ * strings.
+ *   INPUT:  string
+ *   OUTPUT: list<string>
+ ******************************************************/
 list<string> stringListConversion(string s)
 {
 	list<string> filePathList;
@@ -106,11 +157,14 @@ list<string> stringListConversion(string s)
 		position = currentPosition + 1;
 	}
 
-
 	return filePathList;
 }
 
-
+/***********************************************************************
+ * Prompts the user for a positive and a negative integer.
+ * If required input is supplied, the two integers are added and the
+ * sum is displayed.
+ ***********************************************************************/
 int main()
 {
 	/* CAN BE REMOVED (Test of breaking apart the file path)
