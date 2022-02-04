1d91e4cfa301211ccade7e588f217d415456f73b
Emergency-Paperclip <ejnelson1997@gmail.com>
2022-02-04 09:38:11 -0800

    final commit

--- lab05.cpp
+++ lab05.cpp
@@ -1,5 +1,18 @@
-﻿// lab05.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿/***********************************************************************
+ * Program: W05 LAB HOMOGRAPHS
+ *    Week 05, HOMOGRAPHS
+ *    Brother Wilson, CSE 453
+ * Author:
+ *    Ethan Nelson, Josh Donaldson, Benjamin Dyas, Erik Machuca Saez
+ * Summary:
+ *    Compare two strings of file paths to determine if they are
+ * 	homographs or not.
+ *    Estimated: 6 hrs each
+ *    Actual: 7 hrs each
+ * The difficulty was was with using c++. We were a little rusty with this
+ * language. Implementing the functions to determine the homographs was
+ * straightforward.
+ ************************************************************************/
 
 #include <iostream>
 #include <list>
@@ -8,13 +21,15 @@
 #include <vector>
 #include <string.h>
 using namespace std;
-
-void rendering(string e) { cout << e; }
+// prototypes
 string canonicalization(string e, string d);
 bool homograph(string a, string b, string d);
 list<string> stringListConversion(string s);
 
 
+/***********************************************************************
+ * testCase class to hold the strings of the different test cases.
+ ***********************************************************************/
 struct testCase
 {
 	testCase(string a, string b, string d)
@@ -28,6 +43,7 @@ struct testCase
 	string d;
 };
 
+// test cases
 vector<testCase> testCases = {
 	
 	
@@ -43,6 +59,13 @@ vector<testCase> testCases = {
 	testCase("C:\\secret\\password.txt", ".\\..\\..\\secret\\password.txt", "C:\\users\\public")
 };
 
+/******************************************************
+ * METHOD: homograph
+ * receives three strings to compare and determine if
+ * the first two are file path homographs
+ *   INPUT:  (3) strings
+ *   OUTPUT: boolean
+ ******************************************************/
 bool homograph(string a, string b, string d)
 {
 	// string a is the first filepath, b is the seccond, and d is the current directory
@@ -55,6 +78,13 @@ bool homograph(string a, string b, string d)
 	return false;
 }
 
+/******************************************************
+ * METHOD: getString
+ * receives a list of strings and index to
+ * return a string.
+ *   INPUT:  list of strings, int
+ *   OUTPUT: string
+ ******************************************************/
 string getString(list<string> l, int index)
 {
 	auto it = l.begin();
@@ -63,24 +93,37 @@ string getString(list<string> l, int index)
 	return *it;
 }
 
+/******************************************************
+ * METHOD: canonicalization
+ * Uses the following two cannonisation rules to
+ * cannonize the strings into a list of of strings:
+ *
+ * Unique Cannonisation Rule
+ * ∀ e1, e2 C(e1) ≠ C(e2) ↔ H(e1, e2) < p
+ * For all pairs of encodings, if they are not homographs,
+ * they must have different cannonisation
+ *
+ * Reliable Cannonisation Rule
+ * ∀ e1, e2 C(e1) = C(e2) ↔ H(e1, e2) ≥ p
+ * For all pairs of encodings, if they are homographs,
+ * the cannonisation must be the same
+ *
+ *   INPUT:  (2) strings
+ *   OUTPUT: string
+ ******************************************************/
 string canonicalization(string e, string d)
 {
-	// Unique Cannonisation Rule
-	// ∀ e1, e2 C(e1) ≠ C(e2) ↔ H(e1, e2) < p
-	// For all pairs of encodings, if they are not homographs, they must have different cannonisation
 
-	// Reliable Cannonisation Rule
-	// ∀ e1, e2 C(e1) = C(e2) ↔ H(e1, e2) ≥ p
-	// For all pairs of encodings, if they are homographs, the cannonisation must be the same
 	list<string> listE = stringListConversion(e);
 	list<string> listD = stringListConversion(d);
 	list<string> listC = {};
 	string c = "";
+
+	//this for loop constructs the canonicalized string
 	for (int i = 0; i < listE.size(); i++)
 	{
 		
 		string x = getString(listE, i);
-		
 		if (x == "?")
 		{
 			cout << e.substr(e.find("?") + 1) << endl;
@@ -116,6 +159,9 @@ string canonicalization(string e, string d)
 			
 	}
 	int size = listC.size();
+
+
+
 	for (int i = 0; i < size; i++)
 	{
 		if (i != 0)
@@ -128,6 +174,14 @@ string canonicalization(string e, string d)
 	return c;
 }
 
+/******************************************************
+ * METHOD: stringListConversion
+ * receives a file path string and breaks the string
+ * apart from the "\" operator and creates a list of
+ * strings.
+ *   INPUT:  string
+ *   OUTPUT: list<string>
+ ******************************************************/
 list<string> stringListConversion(string s)
 {
 	list<string> filePathList;
@@ -146,19 +200,13 @@ list<string> stringListConversion(string s)
 	return filePathList;
 }
 
-
+/***********************************************************************
+ * Main runs through all of the test cases, then accepts three strings 
+ * from a user and determins if the first two are homographs bassed
+ * on the third.
+ ***********************************************************************/
 int main()
 {
-	/* CAN BE REMOVED (Test of breaking apart the file path)
-	string filePath = "C:\\secret\\password.txt";
-	list<string> filePathList;
-	filePathList = stringListConversion(filePath);
-	for(list<string>::iterator it = filePathList.begin(); it != filePathList.end(); it++)
-	{
-		cout << *it << endl;
-	}
-	cout << "size is" << filePathList.size() << endl;
-	 CAN BE REMOVED end OF file path test */
 
 	string testCase1A = "C:\\secret\\password.txt";
 	string testCase1B = ".\\..\\..\\secret\\password.txt";
